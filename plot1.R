10419b28dc08efc1be82dbd0aaf6c615311fee85
DRAV <vazquez.danran@gmail.com>
2015-11-07 23:05:45 -0600

    Version lista para subirse al coursera

--- plot1.R
+++ plot1.R
@@ -4,10 +4,11 @@ url <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_con
 if( file.exists("power_consumption.zip")) {
   print("El archivo zip existe por lo que procedemos a borrarlo")
   unlink("power_consumption.zip")
-  #Borramos la imagen si existe 
-  if ( file.exists("plot1.png"))
-    unlink("plot1.png")
 } 
+#Borramos la imagen si existe 
+if ( file.exists("plot1.png"))
+  unlink("plot1.png")
+
 print("Descargando el archivo")
 download.file(url, destfile = "power_consumption.zip", method = "curl")
 dateDownloaded <- date()
@@ -24,6 +25,7 @@ power_new <- subset(power, power$Date =="2007-02-01" | power$Date=="2007-02-02")
 #Liberamos memoria
 rm(power)
 #Convertimos variables a numeros ya que las usaremos para las gráficas
+print("Convertimos valores numéricos")
 power_new$Global_active_power <- as.numeric(power_new$Global_active_power)
 power_new$Global_reactive_power <- as.numeric(power_new$Global_reactive_power)
 power_new$Voltage <- as.numeric(power_new$Voltage)
@@ -32,6 +34,10 @@ power_new$Sub_metering_1 <- as.numeric(power_new$Sub_metering_1)
 power_new$Sub_metering_2 <- as.numeric(power_new$Sub_metering_2)
 power_new$Sub_metering_3 <- as.numeric(power_new$Sub_metering_3)
 
+print("Obtenemos la gráfica")
 hist(power_new$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
 dev.copy(png, file="plot1.png", width=480, height=480)
 dev.off()
+print("Borramos archivos")
+unlink("household_power_consumption.txt")
+unlink("power_consumption.zip")
